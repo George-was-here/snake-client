309dbade0d6ccdc431c57c29a8ec5187bc8c3e41
George-was-here <george.burt@gmail.com>
2022-04-14 03:01:16 +0000

    exports & imports integrated between input.js & play.js

--- play.js
+++ play.js
@@ -1,24 +1,9 @@
 const net = require("net");
-const connect = require('./client.js');
+const { connect } = require('./client.js');
+const { setupInput } = require('./input.js');
 
 console.log("Connecting ...");
 
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-};
 
 setupInput();
